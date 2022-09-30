36c2e4083351a3fc55bdd8503be05b2468d45761
ismail baihaqi <113963313+ismaailbaihaqi@users.noreply.github.com>
2022-09-30 13:57:24 +0700

    Add files via upload

---
+++ Praktikum/PrakAlgo 3/Algoritma Elkom1.py
@@ -0,0 +1,20 @@
+print("    ██╗███████╗███╗   ███╗ █████╗ ██╗██╗         ")
+print("    ██║██╔════╝████╗ ████║██╔══██╗██║██║         ")
+print("    ██║███████╗██╔████╔██║███████║██║██║         ")
+print("    ██║╚════██║██║╚██╔╝██║██╔══██║██║██║         ")
+print("    ██║███████║██║ ╚═╝ ██║██║  ██║██║███████╗    ")
+print("    ╚═╝╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ")
+print("                                                 ")
+
+#Ismail Baihaqi
+#065002200025
+a = input("Masukkan angka pertama: ")
+b = input("Masukkan angka kedua  : ")
+c = input("Masukkan angka ketiga : ")
+print()
+if a > b and a > c:
+  print("Angka terbesar adalah: ",a)
+elif b < a and b > c:
+  print("Angka terbesar adalah: ",b)
+else:
+  print("Angka terbesar adalah: ",c)
